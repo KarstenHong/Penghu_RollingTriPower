a97f5dda2fc2af2e05ae65d4bc7dca2ebf440993
KarstenHong <KarstenHong@users.noreply.github.com>
2026-07-27 02:47:42 +0000

    deploy: 9455f895b53159de16d5d352cb933d60ede0e685

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// 請至 Firebase Console > 專案設定 取得您自己的設定值，貼到下面。
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
